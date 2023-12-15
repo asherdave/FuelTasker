e5062780293f4f9450932298d51da3dcc538978b
Kade Sanico <kadesanico@gmail.com>
2023-12-15 23:17:45 +0800

    some changes to buttons pero libog unsa hehe

--- fueltasker/src/Reminder.jsx
+++ fueltasker/src/Reminder.jsx
@@ -35,32 +35,32 @@ export const Reminder = () => {
                             <img className="img-2" alt="Reminder" src={reminder} />
                         </button>
                     </div>
-                    <div className="group-4">
+                    <button className="group-4">
                         <div className="text-wrapper-5">Dashboard</div>
                         <div className="dash-wrapper">
                             <img className="dash" alt="dash" src={dashboard} />
                         </div>
-                    </div>
-                    <div className="group-5">
+                    </button>
+                    <button className="group-5">
                         <div className="text-wrapper-4">Gas Prices</div>
                         <img className="gaspr" alt="Gaspr" src={gasprice} />
-                    </div>
-                    <div className="group-6">
+                    </button>
+                    <button className="group-6">
                         <div className="text-wrapper-6">Monthly Expenses</div>
                         <img className="img" alt="Monthly" src={monthly} />
-                    </div>
-                    <div className="group-7">
+                    </button>
+                    <button className="group-7">
                         <div className="text-wrapper-7">To-Do-List</div>
                         <img className="todolist" alt="Todolist" src={todolist} />
-                    </div>
-                    <div className="group-8">
+                    </button>
+                    <button className="group-8">
                         <div className="text-wrapper-8">Gas Consumption</div>
                         <img className="img-2" alt="Gascon" src={gascon} />
-                    </div>
-                    <div className="group-9">
+                    </button>
+                    <button className="group-9">
                         <div className="text-wrapper-9">Timeline</div>
                         <img className="timeline" alt="Timeline" src={timeline} />
-                    </div>
+                    </button>
                 </div>
                 <div className="overlap-2">
                     <img className="img-3" alt="Reminder" src={reminder} />
