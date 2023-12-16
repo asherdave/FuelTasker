4f1cc66e0915c97681d9371d765a0ec751a9b8e6
Kade Sanico <kadesanico@gmail.com>
2023-12-16 11:56:33 +0800

    gwapo

--- fueltasker/src/CreateReminder.jsx
+++ fueltasker/src/CreateReminder.jsx
@@ -24,43 +24,45 @@ export const CreateReminder = () => {
                         </div>
                     </div>
                     <img className="ellipse" alt="Ellipse" src={profile} />
-                    <div className="group-2">
+                    <button className="group-2">
                         <div className="text-wrapper-3">Logout</div>
-                        <img className="img" alt="Logout" src={logout} />
+                        <img className="img-logout" alt="Logout" src={logout} />
+                    </button>
+                    <div className="group-wrapper">
+                        <button className="group-3">
+                            <div className="text-wrapper-4">Reminder</div>
+                            <img className="img-2" alt="Reminder" src={reminder} />
+                        </button>
                     </div>
-                    <div className="group-3">
-                        <div className="text-wrapper-4">Dashboard</div>
+                    <button className="group-4">
+                        <div className="text-wrapper-5">Dashboard</div>
                         <div className="dash-wrapper">
-                            <img className="dash" alt="Dash" src={dashboard} />
+                            <img className="dash" alt="dash" src={dashboard} />
                         </div>
-                    </div>
-                    <div className="group-4">
-                        <div className="text-wrapper-5">Gas Prices</div>
-                        <img className="img-2" alt="Gaspr" src={gasprice} />
-                    </div>
-                    <div className="group-5">
+                    </button>
+                    <button className="group-5">
+                        <div className="text-wrapper-4">Gas Prices</div>
+                        <img className="gaspr" alt="Gaspr" src={gasprice} />
+                    </button>
+                    <button className="group-6">
                         <div className="text-wrapper-6">Monthly Expenses</div>
                         <img className="img" alt="Monthly" src={monthly} />
-                    </div>
-                    <div className="group-6">
+                    </button>
+                    <button className="group-7">
                         <div className="text-wrapper-7">To-Do-List</div>
                         <img className="todolist" alt="Todolist" src={todolist} />
-                    </div>
-                    <div className="group-7">
+                    </button>
+                    <button className="group-8">
                         <div className="text-wrapper-8">Gas Consumption</div>
                         <img className="img-2" alt="Gascon" src={gascon} />
-                    </div>
-                    <div className="group-8">
-                        <div className="text-wrapper-5">Reminder</div>
-                        <img className="img-2" alt="Reminder" src={reminder} />
-                    </div>
-                    <div className="group-9">
+                    </button>
+                    <button className="group-9">
                         <div className="text-wrapper-9">Timeline</div>
                         <img className="timeline" alt="Timeline" src={timeline} />
-                    </div>
+                    </button>
                 </div>
                 <div className="overlap-2">
-                    <img className="reminder" alt="Reminder" src={reminder} />
+                    <img className="img-3" alt="Reminder" src={reminder} />
                     <img className="rectangle" alt="Rectangle" src={rect99} />
                     <div className="reminder-wrapper">
                         <p className="p">
